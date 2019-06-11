e44119530802faffb5023319c83a305d844670c8
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-06-11 12:47:31 +0300

    6.

--- Field.java
+++ Field.java
@@ -1,7 +1,30 @@
 package com.javarush.task.task22.task2213;
 
 /**
- * поле с клетками
+ * Поле с клетками
+ * Будет отвечать за хранение данных о текущих занятых и свободных клетках на поле игры.
  */
 public class Field {
+
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new int[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
