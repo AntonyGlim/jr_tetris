ff30aff26dd09be95be43cf8357de26a99b467d3
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-06-11 12:30:55 +0300

    3.

--- Tetris.java
+++ Tetris.java
@@ -8,4 +8,12 @@ public class Tetris {
     public static void main(String[] args) {
 
     }
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
 }
